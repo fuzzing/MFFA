f39d21621c36a63eed57e6e46591fd7d68b1d1c1
iblanda <ioan-alexandru.blanda@intel.com>
2015-07-28 12:00:07 +0300

    misc changes
    Signed-off-by: iblanda <ioan-alexandru.blanda@intel.com>

--- get_uniquecrash.py
+++ get_uniquecrash.py
@@ -42,6 +42,10 @@ file_type = sys.argv[3]
 signal_type = sys.argv[4]
 new_crashes = {}
 
+#number of lines to go up in a log to look for a crashing testcase
+
+log_density = 8
+
 #regexp
 
 regex_filename = re.compile("Filename:\S*")
@@ -52,10 +56,10 @@ crash_count = 1
 
 #get the full path to the filename that caused the crash
 
-print log_file
 path = regex_path.findall(log_file)
 path = (str)(path[0])
 path = path.replace("_stagefright", "")
+print "The path to the test files is: " + path
 
 f = open(log_file, "r")
 lines = f.readlines()
@@ -63,112 +67,116 @@ lines = f.readlines()
 #parse every line of the current log
 
 for count in range(0, len(lines)):
-        for crash_line in range(1, 8):
+        if (signal_type in lines[count]): 
+                check_no_lines = count - log_density
+                if check_no_lines < 0:
+                   diff = log_density - count 
+                   check_no_lines = log_density - diff
+                else:
+                   check_no_lines = log_density
+                for crash_line in range(1, check_no_lines):
+                   if ("Filename:" in lines[count - crash_line]):
+                       filename = regex_filename.findall(lines[count-crash_line])
+                       filename = (str)(filename[0])
+                       filename = filename[9:]
+                       break                        
 
-            if (sys.argv[4] in lines[count]):
+		#push the file to the device
 
-                #get the filename that caused a crash
+		cmd = "adb -s " + device_id + " push " + path + "/" \
+		      + filename + " /data/Music"
+		run_subproc(cmd)
 
-                if ("Filename:" in lines[count - crash_line]):
-                    filename = regex_filename.findall(lines[count-1])
-                    filename = (str)(filename[0])
-                    filename = filename[9:]
+		#delete the contents of /data/tombstones from the device
 
-        #push the file to the device
+		cmd = "adb -s " + device_id + " " + "shell rm /data/tombstones/*"
+		run_subproc(cmd)
 
-        cmd = "adb -s " + device_id + " push " + path + "/" \
-              + filename + " /data/Music"
-        run_subproc(cmd)
+		#decode the file on the device
 
-        #delete the contents of /data/tombstones from the device
+		if (file_type == "video"):
+		    cmd = "timeout 15 adb -s " + device_id + " " \
+		          + "shell stagefright /data/Music/" + filename
+		    run_subproc(cmd)
+		if (file_type == "audio"):
+		    cmd = "timeout 15 adb -s " + device_id + " " \
+		          + "shell stagefright -a /data/Music/" + filename
+		    run_subproc(cmd)
 
-        cmd = "adb -s " + device_id + " " + "shell rm /data/tombstones/*"
-        run_subproc(cmd)
+		#remove the file from the device
 
-        #decode the file on the device
+		cmd = "adb -s " + device_id + " " + "shell rm /data/Music/*"
+		run_subproc(cmd)
 
-        if (file_type == "video"):
-            cmd = "timeout 15 adb -s " + device_id + " " \
-                  + "shell stagefright /data/Music/" + filename
-            run_subproc(cmd)
-        if (file_type == "audio"):
-            cmd = "timeout 15 adb -s " + device_id + " " \
-                  + "shell stagefright -a /data/Music/" + filename
-            run_subproc(cmd)
+		#use a try-except construction
+		#for cases when the file did not generate a tombstone
+		#the issue is not reproducible
 
-        #remove the file from the device
+		try:
 
-        cmd = "adb -s " + device_id + " " + "shell rm /data/Music/*"
-        run_subproc(cmd)
+		    #grab the generated tombstone and rename it
 
-        #use a try-except construction
-        #for cases when the file did not generate a tombstone
-        #the issue is not reproducible
+		    tid = (str)(random.random())
+		    tomb_name = "tombstone" + tid
+		    cmd = "adb -s " + device_id + " pull " \
+		          + " /data/tombstones/tombstone_00 " + tomb_name
+		    run_subproc(cmd)
 
-        try:
+		    #parse the tombstone and check for the last accessed PC address
 
-            #grab the generated tombstone and rename it
+		    f = open(tomb_name, "r")
+		    traces = f.readlines()
+		    pc_check = 0
+		    for x in range(0, len(traces)):
+		        if ("backtrace" in traces[x]):
 
-            tid = (str)(random.random())
-            tomb_name = "tombstone" + tid
-            cmd = "adb -s " + device_id + " pull " \
-                  + " /data/tombstones/tombstone_00 " + tomb_name
-            run_subproc(cmd)
+		            #get the pc address from the next line
 
-            #parse the tombstone and check for the last accessed PC address
+		            pc_address = regex_address.findall(traces[x+1])
+		            pc_address = (str)(pc_address[0])
+		            pc_address = pc_address[3:]
+		            print (str)(crash_count) + " -- PC address: " + pc_address
+		            crash_count = crash_count + 1
+		            pc_check = 1
+		            break
+		    if (pc_check == 0):
+		        pc_address = "00000000"
 
-            f = open(tomb_name, "r")
-            traces = f.readlines()
-            pc_check = 0
-            for x in range(0, len(traces)):
-                if ("backtrace" in traces[x]):
+		    #save the file as a new crash(or not) and log the findings
 
-                    #get the pc address from the next line
+		    if (pc_address not in new_crashes.keys()):
+		        new_crashes[pc_address] = filename
+		        print "**NEW**" + pc_address
 
-                    pc_address = regex_address.findall(traces[x+1])
-                    pc_address = (str)(pc_address[0])
-                    pc_address = pc_address[3:]
-                    print (str)(crash_count) + " -- PC address: " + pc_address
-                    crash_count = crash_count + 1
-                    pc_check = 1
-                    break
-            if (pc_check == 0):
-                pc_address = "00000000"
+		        #create a new folder that will contain the tombstone
+		        #and the file that generated the crash
 
-            #save the file as a new crash(or not) and log the findings
+		        cmd = "mkdir issues/" + pc_address
+		        run_subproc(cmd)
 
-            if (pc_address not in new_crashes.keys()):
-                new_crashes[pc_address] = filename
-                print "**NEW**" + pc_address
+		        #copy the tombstone in the new issue folder
 
-                #create a new folder that will contain the tombstone
-                #and the file that generated the crash
+		        cmd = "cp " + tomb_name + " issues/" + pc_address
+		        run_subproc(cmd)
 
-                cmd = "mkdir issues/" + pc_address
-                run_subproc(cmd)
+		        #save the file that caused the crash in the corresponding issue folder
+		        #result: all the files that caused a crash are saved
 
-                #copy the tombstone in the new issue folder
+		        cmd = "find -name " + filename
+		        r = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
+		        file_path = (str)(r.stdout.read())
+		        file_path = file_path.rstrip()
+		        r.wait()
 
-                cmd = "cp " + tomb_name + " issues/" + pc_address
-                run_subproc(cmd)
+		        cmd = "cp " + file_path + " issues/" + pc_address
+		        run_subproc(cmd)
 
-                #save the file that caused the crash in the corresponding issue folder
-                #result: all the files that caused a crash are saved
+		    #delete the gathered tombstone
 
-                cmd = "find -name " + filename
-                r = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
-                file_path = (str)(r.stdout.read())
-                file_path = file_path.rstrip()
-                r.wait()
+		    cmd = "rm " + tomb_name
+		    run_subproc(cmd)
+		    f.close()
+		except IOError:
+		    print "The file did not generate a tombstone..false positive"
+		    continue
 
-                cmd = "cp " + file_path + " issues/" + pc_address
-                run_subproc(cmd)
-
-            #delete the gathered tombstone
-
-            cmd = "rm " + tomb_name
-            run_subproc(cmd)
-            f.close()
-        except IOError:
-            print "The file did not generate a tombstone..false positive"
-            continue
